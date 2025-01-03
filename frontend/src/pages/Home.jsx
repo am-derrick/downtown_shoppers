5dd4716ec63e211980477f00b9913ad3a0c501f6
Derrick Ampire <ampire90@gmail.com>
2025-01-03 11:19:57 +0300

    add: initial home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="py-12">
+      <div className="max-w-7xl mx-auto px-6">
+        <h1 className="text-4xl font-light">Welcome to Downtown Shoppers</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
