cc80d9ec5bee5f8870b94ea68bef2b3a9186145a
Derrick Ampire <ampire90@gmail.com>
2025-01-05 22:49:50 +0300

    modified: added documentation urls for Swagger and ReDoc

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,8 +5,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('shopping.urls')),
+    # API schema URLS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Swagger UI
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # ReDoc UI
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
