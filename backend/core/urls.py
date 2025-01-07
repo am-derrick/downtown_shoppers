8c1a23ac1896519fac52035f2370c22782c73f9d
Derrick Ampire <ampire90@gmail.com>
2025-01-07 20:19:09 +0300

    modified: dashboard app registered

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,6 +14,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('shopping.urls')),
+    path('dashboard/', include('dashboard.urls')),
     # API schema URLS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Swagger UI
