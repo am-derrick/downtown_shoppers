5a50c7f5ddf13c9984af9ad6d765bb9c1a2d8c5d
Derrick Ampire <ampire90@gmail.com>
2025-01-12 15:31:54 +0300

    modified: added credentials while creating the api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
-    headers: {}
+    headers: {},
+    withCredentials: true
 });
 
 export const shoppingListAPI = {
